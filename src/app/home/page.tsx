ec26ff26fbbad4c0ccdbb6dec436c2c4287604fd
Amel Muminovic <amcenp@gmail.com>
2023-11-30 23:05:21 +0100

    feat: added margin

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -212,7 +212,7 @@ const Page = () => {
             <button
               onClick={addProcess}
               type="button"
-              className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2"
+              className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
             >
               <svg
                 className="ww-6 h-5 me-2 -ms-1"
